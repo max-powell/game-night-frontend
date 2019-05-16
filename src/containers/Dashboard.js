a4a833740d17bb00d41518141ede5f4acbb8f821
max-powell <github email address>
2019-05-16 19:43:27 +0100

    created dashboard component w/ grid

---
+++ src/containers/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dashboard = () => (
+  <div id='dashboard'>
+    <div id='test'></div>
+  </div>
+);
+
+export default Dashboard
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Dashboard from './Dashboard';
 
 const MainContainer = () => (
-  <div id='main-container'></div>
+  <div id='main-container'>
+    <Dashboard />
+  </div>
 )
 
 export default MainContainer
