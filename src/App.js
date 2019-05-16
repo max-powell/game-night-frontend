919673a9fdef5c62fdccce13d273daa61886cc21
max-powell <github email address>
2019-05-16 18:27:03 +0100

    initial header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faDiceD20 } from '@fortawesome/free-solid-svg-icons'
+
 import Header from './containers/Header'
 
 import './App.css';
+
+library.add(faDiceD20)
 
 function App() {
   return (
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Header = () => (
   <div id='header'>
-
+    <FontAwesomeIcon icon="dice-d20" size='3x' />
+    <h1 id='header-title'>Game Night</h1>
   </div>
 );
 
