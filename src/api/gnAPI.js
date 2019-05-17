1a2f212a5879db21e5f5b89070fe86de39f8aa8e
max-powell <github email address>
2019-05-17 11:20:25 +0100

    removed original object code

--- src/api/gnAPI.js
+++ src/api/gnAPI.js
@@ -1,14 +1,3 @@
-// const gnAPI = {
-//   _baseUrl: 'http://localhost',
-//   _configBuilder: (method, body) => {
-//     return {
-//       method,
-//       headers: 'application/json'
-//       body: JSON.stringify(body)
-//     }
-//   }
-// }
-
 const gnApi = (() => {
   const _baseUrl = 'http://localhost:3000'
   const _configBuilder = (method, body = '') => {
