915181b14a875fb03ad9783ae35d27d9ee0ee20c
max-powell <github email address>
2019-05-18 12:21:25 +0100

    logout div in header removes token from local storage and redirects to root

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -3,13 +3,18 @@ import '../css/Header.css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const Header = () => (
+const Header = ({history}) => {
 
-  <div id='header'>
+  const logout = () => {
+    localStorage.removeItem('token')
+    history.push('/')
+  }
+
+  return (<div id='header'>
     <FontAwesomeIcon icon="dice-d20" size='2x' />
     <h1>Game Night</h1>
-    {!!localStorage.token && <div>Logout</div>}
-  </div>
-);
+    {!!localStorage.token && <div onClick={logout}>Logout</div>}
+  </div>)
+}
 
 export default Header
