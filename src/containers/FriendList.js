6c9481a8f9fc41503b3d6260313c88fc6726730a
max-powell <github email address>
2019-05-18 20:29:20 +0100

    dashboard shows friends with scrolling

--- src/containers/FriendList.js
+++ src/containers/FriendList.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import FriendListItem from '../components/FriendListItem.js';
+
 const FriendList = ({friends}) => (
   <div className='dashboard-item-list'>
     {
-      friends.map(f => <div>{f.username}</div>)
+      friends.map(f => <FriendListItem friend={f} />)
     }
   </div>
 )
