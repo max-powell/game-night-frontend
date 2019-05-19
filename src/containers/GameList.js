296784db0594788e8ccee765e8e64a298710bba3
max-powell <github email address>
2019-05-19 18:23:05 +0100

    added key to games list items

--- src/containers/GameList.js
+++ src/containers/GameList.js
@@ -4,7 +4,7 @@ import GameListItem from '../components/GameListItem'
 
 const GameList = ({games}) => (
   <div className='dashboard-item-list'>
-    {games.map(g => <GameListItem game={g} />)}
+    {games.map(g => <GameListItem key={g.id} game={g} />)}
   </div>
 )
 
