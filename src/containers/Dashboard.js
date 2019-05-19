50fb5a676a0b347a136f55bb63fa15c35db642ef
max-powell <github email address>
2019-05-19 14:28:56 +0100

    converted dashboard to a class component

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import '../css/Dashboard.css'
 
 import Profile from './Profile'
@@ -6,14 +6,23 @@ import EventDisplay from './EventDisplay'
 import FriendDisplay from './FriendDisplay'
 import GameDisplay from './GameDisplay'
 
-const Dashboard = ({history}) => (
-  <div id='dashboard' className='main-container-item'>
-    {!!localStorage.token || history.push('/')}
-    <Profile />
-    <EventDisplay />
-    <FriendDisplay />
-    <GameDisplay />
-  </div>
-);
+class Dashboard extends Component {
+
+  render() {
+
+    const { history } = this.props;
+
+    return (
+      <div id='dashboard' className='main-container-item'>
+        {!!localStorage.token || history.push('/')}
+        <Profile />
+        <EventDisplay />
+        <FriendDisplay />
+        <GameDisplay />
+      </div>
+    );
+  }
+
+}
 
 export default Dashboard
