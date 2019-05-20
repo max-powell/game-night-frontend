2a7b43018e79bd987a0fb0e206fb30016f0cf890
max-powell <github email address>
2019-05-20 17:46:07 +0100

    added new-event title and area1. location input controls state

--- src/containers/NewEvent.js
+++ src/containers/NewEvent.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Input } from 'semantic-ui-react'
+import '../css/NewEvent.css'
 
 class NewEvent extends Component {
 
@@ -11,10 +13,23 @@ class NewEvent extends Component {
     gameOwner: ''
   }
 
+  handleInputChange = (e, {name, value}) => {
+    this.setState({
+      [name]: value
+    })
+  }
+
   render() {
+
+    const { location, dateTime, invited, notInvited, gameId, gameOwner } = this.state
+    const { handleInputChange } = this
+
     return (
       <div id='new-event' className='main-container-item'>
-        Hi
+        <div id='new-event-title'><h1>Schedule a game night</h1></div>
+          <div id='area1'>
+            <Input label='Location:' name='location' value={location} onChange={handleInputChange} />
+          </div>
       </div>
     )
   }
