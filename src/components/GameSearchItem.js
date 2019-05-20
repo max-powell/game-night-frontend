66701477aabfc142224a8dfb2a9f65fccacde408
max-powell <github email address>
2019-05-20 12:24:53 +0100

    built out display component

--- src/components/GameSearchItem.js
+++ src/components/GameSearchItem.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 const GameSearchItem = ({result}) => (
   <div className='dashboard-search-item'>
-    {result.name}
+    <img src={result.imageUrl} alt='game art' />
+    <div>
+      <p>Name: {result.name}</p>
+      <p>
+      Player range: {
+        result.minPlayers && result.maxPlayers
+        ? `${result.minPlayers}-${result.maxPlayers}`
+        : 'unavailable'
+      }
+      </p>
+      <p>
+      Player range: {
+        result.minPlaytime && result.maxPlaytime
+        ? `${result.minPlaytime}-${result.maxPlaytime}`
+        : 'unavailable'
+      }
+      </p>
+    </div>
   </div>
 )
 
