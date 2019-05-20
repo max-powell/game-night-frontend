e5cb8da4e071903933d98f293437333a14b43ab4
max-powell <github email address>
2019-05-20 14:03:12 +0100

    games show thumbnail

--- src/components/GameListItem.js
+++ src/components/GameListItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const GameListItem = ({game}) => (
   <div className='dashboard-list-item'>
-    <img src='https://imgflip.com/s/meme/Futurama-Fry.jpg' alt='game pic' />
+    <img src={game.imageUrl} alt='game pic' />
     <div>
       <h2>{game.name}</h2>
       <p>Player range: {game.minPlayers}-{game.maxPlayers}</p>
