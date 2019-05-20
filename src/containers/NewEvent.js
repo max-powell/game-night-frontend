154fc3ad8c22405715e90e66af00d06f1df1eae3
max-powell <github email address>
2019-05-20 18:40:31 +0100

    date-time picker linked to datetime in state. css tweaks

--- src/containers/NewEvent.js
+++ src/containers/NewEvent.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { Input } from 'semantic-ui-react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 import '../css/NewEvent.css'
 
 class NewEvent extends Component {
 
   state = {
     location: '',
-    dateTime: '',
+    dateTime: new Date,
     invited: [],
     notInvited: [],
     gameId: 0,
@@ -19,16 +21,38 @@ class NewEvent extends Component {
     })
   }
 
+  test = (...args) => {
+    const state = this.state
+    debugger
+  }
+
+  changeDateTime = dateTime => {this.setState({dateTime})}
+
   render() {
 
     const { location, dateTime, invited, notInvited, gameId, gameOwner } = this.state
-    const { handleInputChange } = this
+    const { handleInputChange, changeDateTime } = this
 
     return (
       <div id='new-event' className='main-container-item'>
         <div id='new-event-title'><h1>Schedule a game night</h1></div>
           <div id='area1'>
-            <Input label='Location:' name='location' value={location} onChange={handleInputChange} />
+            <div>
+            <label>Location: </label>
+              <Input name='location' value={location} onChange={handleInputChange} />
+            </div>
+            <div>
+            <label>When: </label>
+              <DatePicker
+              selected={dateTime}
+              showTimeInput
+              dateFormat='dd/MM/yy h:mm aa'
+              minDate={new Date()}
+              name='dateTime'
+              onChange={changeDateTime}
+              />
+            </div>
+            <div onClick={this.test}>HI</div>
           </div>
       </div>
     )
