d2e8d3fdfb7e54b4ad122f254f1a66d24234caf5
max-powell <github email address>
2019-05-20 18:44:10 +0100

    started invitation component

---
+++ src/containers/Invitations.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Invitations = ({}) => (
+  <div id='invitations'>
+    <div className='invitation-list'>
+    </div>
+    <div className='invitation-list'>
+    </div>
+  </div>
+)
+
+export default Invitations
