94a39b01450fbdc3c4219cb90955067325e0e132
max-powell <github email address>
2019-05-28 18:08:48 +0100

    modal shows location, datetime, attendees and inviteed guests. location and datetime can be changed in state.

--- src/containers/EventModal.js
+++ src/containers/EventModal.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Button, Header, Modal } from 'semantic-ui-react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import gnApi from '../api/gnApi';
+import LocationInput from '../components/LocationInput'
+import DateTimePicker from '../components/DateTimePicker'
 
 class EventModal extends Component {
 
@@ -20,20 +22,55 @@ class EventModal extends Component {
       .then(event => {
         this.setState({
           location: event.location,
-          dateTime: event.dateTime,
+          dateTime: new Date(event.dateTime),
           game: event.game ,
           attendees: event.attendees,
           invited: event.invitedGuests,
           availableGames: event.availableGames,
           host: event['host?']
-        });
+        })
       })
   }
 
+  handleLocationChange = ({target: {value}}) => {this.setState({location: value})}
+
+  changeDateTime = dateTime => {this.setState({dateTime})}
+
   render() {
+
+    const { location, dateTime, attendees, invited } = this.state
+    const { handleLocationChange, changeDateTime } = this
+
     return (
       <Modal trigger={<FontAwesomeIcon icon="ellipsis-h" />} >
-        <Modal.Header>Event Details</Modal.Header>
+        <Modal.Header>
+          Edit Event
+        </Modal.Header>
+        <Modal.Content>
+          <LocationInput
+            location={location}
+            handleLocationChange={handleLocationChange}
+          />
+          <DateTimePicker
+            dateTime={dateTime}
+            changeDateTime={changeDateTime}
+          />
+          <div id='guests'>
+            <label>Going:</label>
+            <ul>
+              {attendees.map(a => <li key={a.id}>{a.username}</li>)}
+            </ul>
+            {
+              invited.length > 0 &&
+              <Fragment>
+                <label>Invited:</label>
+                <ul>
+                  {invited.map(i => <li key={i.id}>{i.username}</li>)}
+                </ul>
+              </Fragment>
+            }
+          </div>
+        </Modal.Content>
       </Modal>
     )
   }
