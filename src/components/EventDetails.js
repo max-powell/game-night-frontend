40a3e3f250f853347f0b263046c839e3d53d2a2a
max-powell <github email address>
2019-05-29 13:59:00 +0100

    added modal css. fixed game name in modal if no game has been selected

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 const EventDetails = ({location, dateTime, attendees, invited, gameId, availableGames}) => (
   <Fragment>
     <p>Location: {location}</p>
-    <p>When: {`${dateTime.toString().match(/\d{2}:\d{2}/)}, ${dateTime.toString().slice(0,3)},${dateTime.toString().slice(3,15)}`}</p>
+    <p>When: {`${dateTime.toString().match(/\d{2}:\d{2}/)} ${dateTime.toString().slice(0,3)},${dateTime.toString().slice(3,15)}`}</p>
     <div id='guests'>
       <label>Going:</label>
       <ul>
@@ -19,7 +19,7 @@ const EventDetails = ({location, dateTime, attendees, invited, gameId, available
         </Fragment>
       }
     </div>
-    <p>Game: {availableGames.find(g => g.id === gameId).name}</p>
+    <p>Game: {gameId ? availableGames.find(g => g.id === gameId).name : 'TBD'}</p>
   </Fragment>
 )
 
