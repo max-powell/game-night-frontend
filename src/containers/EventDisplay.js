579380a245c3663d15e3694e9763128b17c9848b
max-powell <github email address>
2019-06-06 15:29:27 +0100

    tweaked event display's requres for heroku fix

--- src/containers/EventDisplay.js
+++ src/containers/EventDisplay.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import EventDisplayBanner from '../components/EventDisplayBanner'
 import EventList from './EventList'
-import NewEventForm from './NewEventForm'
+import NewEventForm from './NewEventForm.js'
 
 import '../css/EventDisplay.css'
 
