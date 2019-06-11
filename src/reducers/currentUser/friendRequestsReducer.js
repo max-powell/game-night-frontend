77dc68fb16c64a7cbbec866dc52266bdafb17bcf
max-powell <github email address>
2019-06-11 16:43:57 +0100

    added friend request reducer to current user reducer

--- src/reducers/currentUser/friendRequestsReducer.js
+++ src/reducers/currentUser/friendRequestsReducer.js
@@ -1,4 +1,4 @@
-import createReducer from './createReducer'
+import createReducer from '../createReducer'
 
 const removeFriendRequest = (state, action) => {
   return state.filter(fR => fR.id !== action.friendRequest.id)
--- src/reducers/currentUser/index.js
+++ src/reducers/currentUser/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux'
 import gamesReducer from './gamesReducer'
 import friendsReducer from './friendsReducer'
 import eventsReducer from './eventsReducer'
+import friendRequestsReducer from './friendRequestsReducer'
 
 export default combineReducers({
   games: gamesReducer,
   friends: friendsReducer,
-  events: eventsReducer
+  events: eventsReducer,
+  friendRequests: friendRequestsReducer
 })
