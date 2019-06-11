a3cc605008509a649a08a7b01a8f2eddeed3cd21
max-powell <github email address>
2019-06-11 16:40:42 +0100

    added update and leave event handlers with remove and update methods to events handler

--- src/reducers/currentUser/eventsReducer.js
+++ src/reducers/currentUser/eventsReducer.js
@@ -4,7 +4,17 @@ const addEvent = (state, action) => {
   return [...state, action.event]
 }
 
+const removeEvent = (state, action) => {
+  return state.filter(e => e.id !== action.event.id)
+}
+
+const updateEvent = (state, action) => {
+  return [...removeEvent(state,action), action.event]
+}
+
 export default createReducer([], {
   ADD_EVENT: addEvent,
   ACCEPT_EVENT_INVITE: addEvent
+  UPDATE_EVENT: updateEvent,
+  LEAVE_EVENT: removeEvent
 })
