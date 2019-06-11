067227c0008f68e7918ac172da8c4c9718f14b4d
max-powell <github email address>
2019-06-11 20:17:31 +0100

    added set event invites

--- src/reducers/currentUser/eventInvitesReducer.js
+++ src/reducers/currentUser/eventInvitesReducer.js
@@ -4,7 +4,12 @@ const removeEventInvite = (state, action) => {
   return state.filter(eI => eI.id !== action.invite.id)
 }
 
+const setEventInvites = (state, action) => {
+  return action.user.eventInvites
+}
+
 export default createReducer([], {
+  SET_USER: setEventInvites,
   ACCEPT_EVENT_INVITE: removeEventInvite,
   REJECT_EVENT_INVITE: removeEventInvite
 })
