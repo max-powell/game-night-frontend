bc367b065d4533e41e3d37d8d4a9f21ccaa99bf3
max-powell <github email address>
2019-06-12 17:09:19 +0100

    converted dashboard to presentational component

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import '../css/Dashboard.css'
 
 import Profile from './Profile'
@@ -6,52 +6,13 @@ import EventDisplay from './EventDisplay'
 import FriendDisplay from './FriendDisplay'
 import GameDisplay from './GameDisplay'
 
-import gnApi from '../api/gnApi';
-
-class Dashboard extends Component {
-
-  state = {
-    currentUser: {
-      games: [],
-      friends: [],
-      sentFriendRequests: [],
-      events: [],
-      friendRequests: [],
-      eventInvites: []
-    },
-    selectedFriend: {}
-  }
-
-  componentDidMount () {
-    gnApi.getProfile()
-      .then(currentUser => this.setState({currentUser}))
-  }
-
-  selectFriend = selectedFriend => {this.setState({selectedFriend})}
-
-  addGame = game => {
-    this.setState({
-      currentUser: {
-        ...this.state.currentUser,
-        games: [
-          ...this.state.currentUser.games,
-          game
-        ]
-      }
-    })
-  }
-
-  render() {
-    return (
-      <div id='dashboard' className='main-container-item'>
-        <Profile />
-        <EventDisplay />
-        <FriendDisplay />
-        <GameDisplay />
-      </div>
-    );
-  }
-
-}
+const Dashboard = () => (
+  <div id='dashboard' className='main-container-item'>
+    <Profile />
+    <EventDisplay />
+    <FriendDisplay />
+    <GameDisplay />
+  </div>
+)
 
 export default Dashboard
