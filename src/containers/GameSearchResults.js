396f461507266c8cb080a8c927fd60399236dedf
max-powell <github email address>
2019-06-12 17:03:56 +0100

    removed clear selected friend action

--- src/containers/GameSearchResults.js
+++ src/containers/GameSearchResults.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import GameSearchItem from '../components/GameSearchItem';
 
-const GameSearchResults = ({results, addGame, showSearch}) => (
+const GameSearchResults = ({results, addGame}) => (
   <div className='dashboard-search-results'>
     {results.map(r => <GameSearchItem key={r.bgaId} result={r} addGame={addGame} />)}
   </div>
