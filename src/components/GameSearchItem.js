24e5dccb0010add73f6d363d4c6b0cc4ea94a8b9
max-powell <github email address>
2019-06-12 17:05:14 +0100

    converted game search item to a presentational component

--- src/components/GameSearchItem.js
+++ src/components/GameSearchItem.js
@@ -1,38 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-class GameSearchItem extends Component {
-
-  render() {
-
-    const { result, addGame } = this.props
-
-    return (
-      <div className='dashboard-search-item'>
-        <img src={result.imageUrl} alt='game art' />
-        <div>
-          <p>Name: {result.name}</p>
-          <p>
-          Players: {
-            result.minPlayers && result.maxPlayers
-            ? `${result.minPlayers}-${result.maxPlayers}`
-            : 'unavailable'
-          }
-          </p>
-          <p>
-          Playtime: {
-            result.minPlaytime && result.maxPlaytime
-            ? `${result.minPlaytime}-${result.maxPlaytime}`
-            : 'unavailable'
-          }
-          </p>
-        </div>
-        <FontAwesomeIcon icon='plus' onClick={() => addGame(result)}/>
-      </div>
-    )
-  }
-
-}
+const GameSearchItem = ({result, addGame}) => (
+  <div className='dashboard-search-item'>
+    <img src={result.imageUrl} alt='game art' />
+    <div>
+      <p>Name: {result.name}</p>
+      <p>
+      Players: {
+        result.minPlayers && result.maxPlayers
+        ? `${result.minPlayers}-${result.maxPlayers}`
+        : 'unavailable'
+      }
+      </p>
+      <p>
+      Playtime: {
+        result.minPlaytime && result.maxPlaytime
+        ? `${result.minPlaytime}-${result.maxPlaytime}`
+        : 'unavailable'
+      }
+      </p>
+    </div>
+    <FontAwesomeIcon icon='plus' onClick={() => addGame(result)}/>
+  </div>
+)
 
 export default GameSearchItem
