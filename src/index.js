4e4d71aa31e68f3be3f6b4bec3feea938fd4250e
max-powell <github email address>
2019-07-27 17:36:00 +0100

    fixed store creator to use redux devtools compose

--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ import rootReducer from './reducers/index'
 
 import App from './App'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(thunk)
   )
 )
 
